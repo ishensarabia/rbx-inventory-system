b53cd71a2ef4f3f1aa6cb04e8540c8ba358b2049
Ishen Sarabia <81144904+ishensarabia@users.noreply.github.com>
2025-02-22 00:24:59 -0600

    Create ItemsEnum.lua

---
+++ src/ReplicatedStorage/Enums/ItemsEnum.lua
@@ -0,0 +1,6 @@
+local ItemEnum = {
+	[1] = { Name = "Guitar", Description = "An electric guitar." },
+	[2] = { Name = "Microphone", Description = "A good mic for recording sessions." },
+}
+
+return ItemEnum
