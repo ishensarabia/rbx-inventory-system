27833cf2a54aa977f6d07073a4fcfa11b8a69b21
Ishen Sarabia <81144904+ishensarabia@users.noreply.github.com>
2025-02-22 14:06:09 -0600

    Update ItemsEnum.lua

--- src/ReplicatedStorage/Enums/ItemsEnum.lua
+++ src/ReplicatedStorage/Enums/ItemsEnum.lua
@@ -1,6 +1,13 @@
 local ItemEnum = {
-	[1] = { Name = "Guitar", Description = "An electric guitar." },
-	[2] = { Name = "Microphone", Description = "A good mic for recording sessions." },
+	[1] = { Name = "Guitar", Description = "An electric guitar.", Image = "rbxassetid://16901783930" },
+	[2] = {
+		Name = "Microphone",
+		Description = "A good mic for recording sessions.",
+		Image = "rbxassetid://16179453939",
+	},
+	[3] = { Name = "Drums", Description = "Itama drums.", Image = "rbxassetid://16901987665" },
+	[4] = { Name = "Bass", Description = "Fender bass.", Image = "rbxassetid://13775183461" },
+	[5] = { Name = "Notebook", Description = "Notebook to write some songs.", Image = "rbxassetid://13997044681" },
 }
 
 return ItemEnum
