5628399eb80f85ac172fcc78f6550eb0762650d3
Ishen Sarabia <81144904+ishensarabia@users.noreply.github.com>
2025-02-22 14:13:00 -0600

    Update KnitClient.client.lua

--- src/StarterPlayerScripts/KnitClient.client.lua
+++ src/StarterPlayerScripts/KnitClient.client.lua
@@ -2,14 +2,10 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local StarterPlayerScripts = game:GetService("StarterPlayer").StarterPlayerScripts
 local Knit = require(ReplicatedStorage.Packages.Knit)
 
-
 for _, controller in (StarterPlayerScripts:GetDescendants()) do
-    if controller:IsA("ModuleScript") and controller.Name:match(".+Controller$") then
-        require(controller)
-    end
+	if controller:IsA("ModuleScript") and controller.Name:match(".+Controller$") then
+		require(controller)
+	end
 end
 
-
-Knit.Start():andThen(function()
-    -- print("Knit client started")
-end):catch(warn)
+Knit.Start():andThen(function() end):catch(warn)
