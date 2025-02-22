fd49690e66bf73bf5c03ddab0aa0973397b16126
Ishen Sarabia <81144904+ishensarabia@users.noreply.github.com>
2025-02-22 00:25:09 -0600

    Add components

--- src/ServerScriptService/KnitServer.server.lua
+++ src/ServerScriptService/KnitServer.server.lua
@@ -1,13 +1,21 @@
 -- Roblox services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerStorage = game:GetService("ServerStorage")
+local Knit = require(ReplicatedStorage.Packages.Knit)
+local Component = require(ReplicatedStorage.Packages.Component)
 -- Knit
 local Knit = require(ReplicatedStorage.Packages.Knit)
 
--- Initialize services
+-- Initialize services & components
 for _, service in (ServerStorage.Source.Services:GetDescendants()) do
     if service:IsA("ModuleScript") and service.Name:match(".+Service$") then
         require(service)
+    end
+end
+
+for _, component in (ServerStorage.Source.Components:GetDescendants()) do
+    if component:IsA("ModuleScript") then
+        require(component)
     end
 end
 
